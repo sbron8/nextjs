9cf7adac12381c65fadc20664ea414eba7a318a9
sbron8 <90700164+sbron8@users.noreply.github.com>
2021-11-14 14:54:47 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+
+function storedata(){
+  let fname = document.getElementById("fname").value;
+  let lname = document.getElementById("lname").value;
+  let phonenum = document.getElementById("phonenum").value;
+  let tnameA = document.getElementById("tnameA").value;
+  let tnameB = document.getElementById("tnameB").value;
+  let tnameC = document.getElementById("tnameC").value;
+  let tnameD = document.getElementById("tnameD").value;
+  let tnameE = document.getElementById("tnameE").value;
+  let tnameF = document.getElementById("tnameF").value;
+  let tnameG = document.getElementById("tnameG").value;
+  
+  console.log(fname, lname, phonenum, tnameA, tnameB, tnameC, tnameD, tnameE, tnameF, tnameG)
+
+}
+
